d7c08569faeadac1e18e8e47e84ed793af34c868
VLIGS <liudmila.veshneva@gmail.com>
2016-01-03 18:46:20 +0000

    implemented DictionaryBST, passed tests

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -9,33 +9,40 @@ import java.util.TreeSet;
 public class DictionaryBST implements Dictionary 
 {
    private TreeSet<String> dict;
-	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
-	
-    
+
+    public DictionaryBST(){
+        dict = new TreeSet<String>();
+    }
+
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+    	word = word.toLowerCase();
+        if(dict.contains(word)) {
+            return false;
+        }
+        else {
+            dict.add(word);
+            return true;
+        }
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+    	if(dict.contains(s.toLowerCase())) {
+            return true;
+        }
+        else return false;
     }
 
 }
