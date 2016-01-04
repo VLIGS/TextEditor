82e5cdf47c050d9950ba14b9bcfa08d5b44ed247
VLIGS <liudmila.veshneva@gmail.com>
2016-01-04 19:02:27 +0000

    three tests passed

--- src/spelling/AutoCompleteDictionaryTrieTester.java
+++ src/spelling/AutoCompleteDictionaryTrieTester.java
@@ -59,31 +59,28 @@ public class AutoCompleteDictionaryTrieTester {
 	public void testIsWord()
 	{
 		assertEquals("Testing isWord on empty: Hello", false, emptyDict.isWord("Hello"));
-		//assertEquals("Testing isWord on small: Hello", true, smallDict.isWord("Hello"));
-		//assertEquals("Testing isWord on large: Hello", true, largeDict.isWord("Hello"));
+		assertEquals("Testing isWord on small: Hello", true, smallDict.isWord("Hello"));
+		assertEquals("Testing isWord on large: Hello", true, largeDict.isWord("Hello"));
 		
-		//assertEquals("Testing isWord on small: hello", true, smallDict.isWord("hello"));
-		//assertEquals("Testing isWord on large: hello", true, largeDict.isWord("hello"));
+		assertEquals("Testing isWord on small: hello", true, smallDict.isWord("hello"));
+		assertEquals("Testing isWord on large: hello", true, largeDict.isWord("hello"));
 
-		//assertEquals("Testing isWord on small: hellow", false, smallDict.isWord("hellow"));
-		//assertEquals("Testing isWord on large: hellow", false, largeDict.isWord("hellow"));
+		assertEquals("Testing isWord on small: hellow", false, smallDict.isWord("hellow"));
+		assertEquals("Testing isWord on large: hellow", false, largeDict.isWord("hellow"));
 		
 		assertEquals("Testing isWord on empty: empty string", false, emptyDict.isWord(""));
-		//assertEquals("Testing isWord on small: empty string", false, smallDict.isWord(""));
-		//assertEquals("Testing isWord on large: empty string", false, largeDict.isWord(""));
+		assertEquals("Testing isWord on small: empty string", false, smallDict.isWord(""));
+		assertEquals("Testing isWord on large: empty string", false, largeDict.isWord(""));
 		
-		//assertEquals("Testing isWord on small: no", false, smallDict.isWord("no"));
-		//assertEquals("Testing isWord on large: no", true, largeDict.isWord("no"));
+		assertEquals("Testing isWord on small: no", false, smallDict.isWord("no"));
+		assertEquals("Testing isWord on large: no", true, largeDict.isWord("no"));
 		
-		//assertEquals("Testing isWord on small: subsequent", true, smallDict.isWord("subsequent"));
-		//assertEquals("Testing isWord on large: subsequent", true, largeDict.isWord("subsequent"));
-		
-		
+		assertEquals("Testing isWord on small: subsequent", true, smallDict.isWord("subsequent"));
+		assertEquals("Testing isWord on large: subsequent", true, largeDict.isWord("subsequent"));
 	}
 
 
 	/** Test the addWord method */
-	/**
 	@Test
 	public void addWord()
 	{
@@ -123,7 +120,6 @@ public class AutoCompleteDictionaryTrieTester {
 		assertEquals("Testing isWord on small: subsequent", true, smallDict.isWord("subsequent"));
 		assertEquals("Testing isWord on large: subsequent", true, largeDict.isWord("subsequent"));
 	}
-	**/
 
 	/**
 	@Test
